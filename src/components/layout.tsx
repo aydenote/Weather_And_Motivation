e0f067055acb9ee1692fa2a92aba60451f4b3cc7
aydenote <dslgpgh@gmail.com>
2023-04-09 00:43:07 +0900

    레이아웃 컴포넌트 구현

---
+++ src/components/layout.tsx
@@ -0,0 +1,12 @@
+import Header from './header';
+import Footer from './footer';
+
+export default function Layout({ children }: any) {
+  return (
+    <div className="bg-primary">
+      <Header />
+      <div>{children}</div>
+      <Footer />
+    </div>
+  );
+}
