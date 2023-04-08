0e5279cade5c4cd8c6f98979306a82272295c71a
aydenote <dslgpgh@gmail.com>
2023-04-09 00:42:06 +0900

    푸터 컴포넌트 구현

---
+++ src/components/footer.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import blogSrc from '../../public/blog.svg'
+import githubSrc from '../../public/github.svg'
+
+export default function Footer() {
+  return (
+    <footer className="text-gray-600 body-font">
+      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+          <span className="ml-3 text-xl">오늘 너와 날씨는 어때?</span>
+        </a>
+        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+          © 2023 ayden — project
+        </p>
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+          <Link href="https://velog.io/@aydenote" target='_blank' className="ml-3 text-gray-500">
+            <Image width={20} height={20} src={blogSrc} alt='link to blog' className='w-5 h-5' />
+          </Link>
+          <Link href="https://github.com/aydenote" target='_blank' className="ml-3 text-gray-500">
+            <Image width={20} height={20} src={githubSrc} alt='link to blog' className='w-5 h-5' />
+          </Link>
+        </span>
+      </div>
+    </footer>
+  );
+}
