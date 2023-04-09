8be51ae8a5e9b675843f71a9e1bd46acfa08f6e5
aydenote <dslgpgh@gmail.com>
2023-04-09 20:37:11 +0900

    헤더 로고 및 타이틀 링크 경로 추가

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,10 +7,10 @@ export default function Header() {
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <Link href='/' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <Image width={50} height={50} src={logoSrc} alt='logo' />
-          <span className="ml-3 text-xl">날씨와 동기의 관계</span>
-        </a>
+          <span className="ml-3 text-xl">기분 좋은 날씨</span>
+        </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <Link href="/" className="mr-5 hover:text-gray-900">
             홈
