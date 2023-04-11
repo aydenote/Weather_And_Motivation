1c49df2fe38bdc2b939ec57b637fbecd608622ea
aydenote <dslgpgh@gmail.com>
2023-04-11 23:19:10 +0900

    메인 페이지 구글 로그인 기능 구현

--- src/components/home/main.tsx
+++ src/components/home/main.tsx
@@ -1,8 +1,47 @@
+import { useEffect, useState } from 'react';
 import { MainAnimation } from '@/components/animation';
-
+import { collection, addDoc } from "firebase/firestore";
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth, db } from '../../../firebase';
+import { loginType } from '../../../type';
 
 export default function Hero() {
+  const [token, setToken] = useState<string | null>(null);
 
+  /** firestore에 계정 컬렉션 추가 함수*/
+  async function addDatabase() {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        todoList: [
+          {
+            state: "",
+            text: "",
+            completeDate: ""
+          }
+        ]
+      }
+      );
+      console.log("Document written with ID: ", docRef.id);
+      localStorage.setItem("dbId", docRef.id);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  }
+
+
+  /** 구글 로그인 함수 */
+  async function handleLogin() {
+    const provider = new GoogleAuthProvider();
+    const signIn = await signInWithPopup(auth, provider);
+    const userInfo = signIn.user as loginType;
+    userInfo.accessToken && localStorage.setItem('token', userInfo.accessToken);
+    setToken(localStorage.getItem('token'));
+    !localStorage.getItem('dbId') && addDatabase();
+  }
+
+  useEffect(() => {
+    setToken(localStorage.getItem('token'));
+  }, [])
   return (
     <>
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
@@ -17,7 +56,7 @@ export default function Hero() {
           본인이 어느 유형인지 파악해보세요!
         </p>
         <div className="flex justify-center">
-          <button className="btn-project">구글 계정으로 로그인</button>
+          {!token && <button className="btn-project" onClick={handleLogin}>구글 계정으로 로그인</button>}
         </div>
       </div>
       <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
