5cd4ac2f0f1d14793b0a49d5982d40d3d0e08783
aydenote <dslgpgh@gmail.com>
2023-04-11 23:06:01 +0900

    연락 페이지 애니메이션 파일 추가

--- src/components/animation.tsx
+++ src/components/animation.tsx
@@ -1,8 +1,21 @@
 import Lottie from 'react-lottie-player';
 import mainJson from '../../public/main.json';
-
+import callJson from '../../public/contact-call.json';
+import emailJson from '../../public/contact-email.json';
+import blogJson from '../../public/contact-blog.json';
 
 export function MainAnimation() {
   return <Lottie loop animationData={mainJson} play style={{ margin: 'auto' }} />;
 }
 
+export function CallAnimation() {
+  return <Lottie loop animationData={callJson} play style={{ margin: 'auto', borderBottom: '1px solid grey' }} />;
+}
+
+export function EmailAnimation() {
+  return <Lottie loop animationData={emailJson} play style={{ margin: 'auto', borderBottom: '1px solid grey' }} />;
+}
+
+export function BlogAnimation() {
+  return <Lottie loop animationData={blogJson} play style={{ margin: 'auto', borderBottom: '1px solid grey' }} />;
+}
