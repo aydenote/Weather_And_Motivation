85ac5d1856a79f98f7b1035130d70ae38bb9891a
aydenote <dslgpgh@gmail.com>
2023-04-14 03:05:44 +0900

    일정 추가, 삭제, 상태 변경 redux 구현

--- src/redux/todo.ts
+++ src/redux/todo.ts
@@ -1,27 +1,41 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-import { todoState } from '../../type';
+import { createSlice, PayloadAction, configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import { todoType, todosState, toggleType } from '../../type';
 
-const initialState: todoState = {
-  completeDate: 0,
-  text: '',
-  state: '',
+const initialState: todosState = {
+  todos: [],
 };
 
-const todoSlice = createSlice({
-  name: 'todo',
+const todosSlice = createSlice({
+  name: 'todos',
   initialState,
   reducers: {
-    SET(state) {},
-    ADD(state) {},
-    UPDATE(state) {},
-    DELELTE(state) {},
+    setTodos: (state, action: PayloadAction<todoType[]>) => {
+      state.todos = action.payload;
+    },
+    addTodo: (state, action: PayloadAction<todoType>) => {
+      state.todos.push(action.payload);
+    },
+    toggleTodo: (state, action: PayloadAction<toggleType>) => {
+      const { id, completeDate } = action.payload;
+      const todo = state.todos.find(todo => todo.id === id);
+      if (todo) {
+        todo.completed = !todo.completed;
+        todo.completeDate = completeDate;
+      }
+    },
+    deleteTodo: (state, action: PayloadAction<string>) => {
+      const index = state.todos.findIndex(todo => todo.id === action.payload);
+      if (index !== -1) {
+        state.todos.splice(index, 1);
+      }
+    },
   },
 });
 
-export const { SET, ADD, UPDATE, DELELTE } = todoSlice.actions;
+export const { setTodos, addTodo, toggleTodo, deleteTodo } = todosSlice.actions;
 
 export default configureStore({
-  reducer: {
-    todo: todoSlice.reducer,
-  },
+  reducer: todosSlice.reducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
--- type.ts
+++ type.ts
@@ -3,3 +3,19 @@ export type loginType = {
   email: string;
   displayName: string;
 };
+
+export type todoType = {
+  id: string;
+  text: string;
+  completed: boolean;
+  completeDate: string;
+};
+
+export type todosState = {
+  todos: todoType[];
+};
+
+export type toggleType = {
+  id: string;
+  completeDate: string;
+};
