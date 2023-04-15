8dbc9f090e56a48b3daa9122212a5301faad8d78
aydenote <dslgpgh@gmail.com>
2023-04-15 18:28:00 +0900

    redux-persist 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 import store from '../redux/todo'
 import '@/styles/globals.css'
+
+const persistor = persistStore(store);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   )
 }
-
