e088ab81b2f916102bee3989a97e25a8d34ba21a
aydenote <dslgpgh@gmail.com>
2023-04-16 19:40:38 +0900

    일정 저장 애니메이션 구현

--- src/components/animation.tsx
+++ src/components/animation.tsx
@@ -4,6 +4,7 @@ import callJson from '../../public/contact-call.json';
 import emailJson from '../../public/contact-email.json';
 import blogJson from '../../public/contact-blog.json';
 import notJson from '../../public/not-found.json'
+import successJson from '../../public/successful.json'
 
 export function MainAnimation() {
   return <Lottie loop animationData={mainJson} play style={{ margin: 'auto' }} />;
@@ -23,4 +24,8 @@ export function BlogAnimation() {
 
 export function NotFoundAnimation() {
   return <Lottie loop animationData={notJson} play style={{ margin: 'auto', borderBottom: '1px solid grey' }} />;
+}
+
+export function SuccessAnimation() {
+  return <Lottie loop animationData={successJson} play />;
 }
