6c220eb11d1e7c62b385eff90f59e1fb51596710
aydenote <dslgpgh@gmail.com>
2023-04-18 18:12:40 +0900

    기상청 API 기본 URL 수정

--- src/pages/api/weather.ts
+++ src/pages/api/weather.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export function getForecast(date: string) {
-  const endPointUrl = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst';
+  const endPointUrl = 'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst';
   return axios({
     url: `${endPointUrl}?ServiceKey=${process.env.NEXT_PUBLIC_WEATHER_API}&numOfRows=10&pageNo=1&dataType=JSON&base_date=${date}&base_time=0500&nx=55&ny=127`,
     method: 'get',
